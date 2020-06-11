fb862295d1d264e73fdf3a3335685c7966c86024
Choukriel <choukrielou@gmail.com>
2020-06-11 04:17:20 +0100

    Module 4 Assingement Solution

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,18 @@
+
+
+
+//byeSpeak
+
+
+(function(window){
+
+	var speakWord = "Good bye";
+
+	var byeSpeaker = {};
+
+	byeSpeaker.speak = function(name){
+			console.log(speakWord + " " + name);
+	}
+	window.byeSpeaker = byeSpeaker;
+}
+)(window);
